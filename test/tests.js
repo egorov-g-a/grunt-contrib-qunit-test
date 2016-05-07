c23d48fb74e944431f60245663796775be2297f7
Gennadij Egorov <egorov_g_a@mail.ru>
2016-05-07 15:25:25 +0400

    Added another test

--- test/tests.js
+++ test/tests.js
@@ -1,5 +1,10 @@
 
-var qu = 
 QUnit.test( "hello test", function( assert ) {
   assert.ok( 1 == "1", "Passed!" );
+});
+
+
+QUnit.test("Test function trim", function(assert){
+	var res = trim(" hello world ");
+	assert.equal(res,"hello world","Passed!");
 });
